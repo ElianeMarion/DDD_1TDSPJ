f9958d4da6a6d7bdbc3373176b7fcb79cec54b1d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-16 07:48:29 -0300

    Adição de classes

---
+++ listas/src/tests/TesteEpisodios.java
@@ -0,0 +1,21 @@
+package tests;
+
+import models.Episodio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteEpisodios {
+    public static void main(String[] args) {
+        Episodio episodio1 = new Episodio(1, "Pilot", 8.5, 1000);
+        Episodio episodio2 = new Episodio(2, "Lucifer, Stay. Good Devil", 8.0, 1000);
+        Episodio episodio3 = new Episodio(3, "The Would-Be Prince of Darkness", 7.5, 1000);
+        Episodio episodio4 = new Episodio(4, "Manly Whatnots", 8.3, 1000);
+        Episodio episodio5 = new Episodio(1, "Everything's Coming Up Lucifer", 8.5, 1000);
+        Episodio episodio6 = new Episodio(2, "Liar, Liar, Slutty Dress on Fire", 0.0, 1000);
+        Episodio episodio7 = new Episodio(3, "Sin-Eater", 7.9, 1000);
+        Episodio episodio8 = new Episodio(4, "Lady Parts", 8.3, 1000);
+
+      
+    }
+}
--- listas/src/tests/TesteList.java
+++ listas/src/tests/TesteList.java
@@ -1,3 +1,5 @@
+package tests;
+
 public class TesteList {
     public static void main(String[] args) {
 
