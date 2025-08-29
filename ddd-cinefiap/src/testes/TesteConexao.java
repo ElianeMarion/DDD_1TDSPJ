3d9919125fd2562220886ce573ea2e2b66c3c2a9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-29 08:46:40 -0300

    Projeto cinefiap

---
+++ ddd-cinefiap/src/testes/TesteConexao.java
@@ -0,0 +1,15 @@
+package testes;
+
+
+import dao.ConnectionFactory;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+        System.out.println("Teste de conexão com o banco de dados");
+        if(ConnectionFactory.obterConexao() == null){
+            System.out.println("Erro ao estabelecer a conexão");
+        }else{
+            System.out.println("Conexão foi estabelecida com sucesso!");
+        }
+    }
+}
